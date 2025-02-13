4994fc2eb348b666e3274873a7d01106eba3d7ff
Lucas Yuki Nishimoto <lucas.yuki@unesp.br>
2025-02-13 18:53:44 -0300

    Subindo projeto com tudo funcionando, exceto o backgound que ta com um bug

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import AddTask from "./componentes/AddTask";
+import Tasks from "./componentes/Tasks";
+import { v4 } from "uuid";
+import Title from "./componentes/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  //SE FOSSE USAR API SERIA AQUI
+  /*useEffect(() => {
+    const fetchTasks = async () => {
+      // chamar a API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+
+      // pegar os dados que ela retorna
+      const data = await response.json();
+
+      // armazenar ou persistir esses dados no state
+      setTasks(data);
+    };
+    //CASO EU QUEIRA USAR A API
+    //fetchTasks();
+  }, []);*/
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      //NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function onTrashClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title: title,
+      description: description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onTrashClick={onTrashClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
